670b7e6cf24f0514bad7d2ca5892548236bdaa92
Martin Otyeka <otyeka.martin@gmail.com>
2018-06-14 13:53:10 -0400

    Initial Commit

---
+++ SnapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapKit
+//
+//  Created by Martin Otyeka on 2018-06-14.
+//  Copyright © 2018 New Capsule Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
