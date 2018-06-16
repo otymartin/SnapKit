df1bfedd2bb4b5c3a18d13cf20f87985c2a50a61
Martin Otyeka <otyeka.martin@gmail.com>
2018-06-16 18:39:59 -0400

    Stuff

--- SnapKit/ViewController.swift
+++ SnapKit/ViewController.swift
@@ -14,12 +14,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let sticker = SCSDKSnapSticker(stickerImage: #imageLiteral(resourceName: "sticker.png"))
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
+        button.setTitle("Start Snapping", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .heavy)
+        self.view.addSubview(button)
+        button.center = self.view.center
+        button.addTarget(self, action: #selector(self.tap), for: .touchUpInside)
+    }
+    
+    @objc func tap() {
         
-        let snap = SCSDKNoSnapContent()
-        snap.sticker = sticker
-        snap.caption = "Snap on Snapchat!"
-        snap.attachmentUrl = "https://snapchat.com"
+        let photo = SCSDKSnapPhoto(image: #imageLiteral(resourceName: "me.jpg"))
+        
+        let snap = SCSDKPhotoSnapContent(snapPhoto: photo)
+        snap.attachmentUrl = "https://newcapsulecorp.com"
         
         let api = SCSDKSnapAPI(content: snap)
         api.startSnapping { (error) in
